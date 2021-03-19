42fceed639a3e74e0cbf6efa13c9968fd6c6c9c4
herbrant <cdavide98carnemolla@gmail.com>
2021-03-19 10:16:14 +0100

    Add 6b.sql

---
+++ 6b.sql
@@ -0,0 +1,1 @@
+SELECT staff.first_name, staff.last_name, SUM(amount) FROM staff JOIN payment ON staff.staff_id = payment.staff_id WHERE payment.payment_date BETWEEN '2005-08-01' AND '2005-08-31' GROUP BY staff.staff_id;
