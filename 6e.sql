85e5f4629cd92a0c2745b3e8a0cb805a59ea1386
herbrant <cdavide98carnemolla@gmail.com>
2021-03-19 10:30:45 +0100

    Add 6e.sql

---
+++ 6e.sql
@@ -0,0 +1,1 @@
+SELECT customer.first_name, customer.last_name, SUM(payment.amount) FROM customer JOIN payment ON customer.customer_id = payment.customer_id GROUP BY customer.customer_id ORDER BY customer.last_name;
